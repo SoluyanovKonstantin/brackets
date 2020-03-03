07e79be7a0a08068b3dce1be0e11de2e8d9ac054
Soluyanov Konstantin <dr.chtec@yandex.ru>
2020-03-03 21:29:06 +0400

    feat: resolve brackets

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let innerStr = str;
+  let i = 0;
+  while (i < innerStr.length) {
+    let j = 0;
+    while (j < bracketsConfig.length) {
+      if (innerStr[i+1] === bracketsConfig[j][1]){ 
+        if (innerStr[i] === bracketsConfig[j][0]) {
+          innerStr = innerStr.substring(0, i) + innerStr.substring(i+2);
+          i--;
+          j = 0;
+        } else i++;
+      }
+      else if (innerStr[i+1] === bracketsConfig[j][0]) {
+        i++;
+        j = 0;
+      }
+      else j++;
+    }
+    i++;
+  }
+  return innerStr.length > 0 ? false : true;
 }
